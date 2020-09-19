76aafb913dfebdfdc86b1186be29b1fce403b1ef
Brock Byrd <brockbyrdd@gmail.com>
2020-09-19 16:45:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,14 +3,30 @@ require 'pry'
 class CashRegister
 
   def initialize
-
-    total = 0
-    apply_discount
+    
   end
 
   def total
     total
   end
+
+  def add_item
+    
+  end
+
+  def apply_discount
+    
+  end
+
+  def items
+    
+  end
+
+  def void_last_transaction
+    
+  end
+
+  
 
 
 end
