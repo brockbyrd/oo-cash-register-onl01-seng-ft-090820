63ca9750a384c2d44f1b95d079a3e5ca20835fd9
Brock Byrd <brockbyrdd@gmail.com>
2020-09-19 17:36:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,48 +1,54 @@
 require 'pry'
 
-class CashRegister
-
-    attr_accessor :total, :discount, :last_transaction
-
-    def initialize(discount = 0)
+    class CashRegister
+      attr_accessor :discount, :total, :last_transaction, :cart
+    
+    
+      def initialize(discount = 0)
         @total = 0
         @discount = discount
-        @items = []
+        @cart = []
         @last_transaction = nil
         @last_transaction_name = nil
-    end
-
-    def add_item(title, price, quantity = 1)
+      end
+    
+      def add_item(item, price, quantity = 1)
         i = 0
-        while i < quantity do
-            @items << title
-            @last_transaction = price * quantity
-            @last_transaction_name = title
-            i += 1
-        end
-        self.total += price * quantity
-    end
-
-    def apply_discount
-        if self.discount == 0
-            "There is no discount to apply."
+              while i < quantity do
+                  @items << title
+                  @last_transaction = price * quantity
+                  @last_transaction_name = title
+                  i += 1
+              end
+              self.total += price * quantity
+      end
+    
+      def apply_discount
+        if discount != 0
+          self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+          "After the discount, the total comes to $#{self.total}."
         else
-            self.total -= (self.total * (self.discount / 100.0))
-            "After the discount, the total comes to $#{self.total.round}."
+          "There is no discount to apply."
         end
-    end
-
-    def items
-        @items
-    end
-
-    def void_last_transaction
-        items.delete(@last_transaction_name)
-        if items.empty?
-            self.total = 0.0
+      end
+    
+      def items
+        item_names = []
+        @cart.each do | item_info |
+          for qty in 1..item_info[:quantity]
+            item_names << item_info[:name]
+          end
+        end
+        item_names
+      end
+    
+      def void_last_transaction
+        cart.delete(@last_transaction_name)
+        if cart.empty?
+          self.total = 0.0
         else
-            self.total -= @last_transaction
+          self.total -= @last_transaction
         end
-    end
+      end
 
 end
